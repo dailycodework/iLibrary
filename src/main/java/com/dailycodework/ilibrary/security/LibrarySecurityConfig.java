3bd13394edd2b4555d6b8c13624544f5bdb7add9
softpager <deprogramminglord@gmail.com>
2023-03-05 05:05:03 +0700

    ilibrary jwt start up project

--- src/main/java/com/dailycodework/ilibrary/security/LibrarySecurityConfig.java
+++ src/main/java/com/dailycodework/ilibrary/security/LibrarySecurityConfig.java
@@ -1,21 +1,15 @@
 package com.dailycodework.ilibrary.security;
 
-import com.dailycodework.ilibrary.jwt.JWTAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 /**
  * @author Samson Effes
@@ -33,25 +27,9 @@ public class LibrarySecurityConfig {
             "/authenticate/**"
     };
 
-    @Autowired
-    private JWTAuthenticationFilter authenticationFilter;
-
-    @Autowired
-    private LibraryUserDetailsService userDetailsService;
-
-
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        var authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-       return authenticationProvider;
     }
 
     @Bean
@@ -61,16 +39,8 @@ public class LibrarySecurityConfig {
                 .requestMatchers(UN_SECURED_URLs).permitAll().and()
                 .authorizeHttpRequests().requestMatchers(SECURED_URLs)
                 .hasAuthority("ADMIN").anyRequest().authenticated()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .and().httpBasic().and()
                 .build();
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
     }
 
 }
